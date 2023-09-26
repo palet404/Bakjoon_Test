454085436207f30bc9b94ab8936f62c4b3b47e16
palet404 <telap9909@gmail.com>
2023-09-26 21:48:05 +0900

    Try2

--- LV1_10869.py
+++ LV1_10869.py
@@ -1,10 +1,18 @@
+# Write a program returns four basic operation
+# After receiving the two integers A and B.
+# Condition : A >= 1 , B <= 10^4
+
 A, B = map(int, input().split())
 
 if A >= 1 and B <= 10^4 :
     print(A+B)
     print(A-B)
-    print(A//B)
+    print(A*B)
+    print(int(A/B))
     print(A%B)
 
 else :
     print("A and B should integer, \n A is more than 0, B is not more than 10^4")
+
+# A//B round down number ex) -1/2 => -0.5 => -1
+# int(A/B) drop the decimal point -1/2 => -0.5 => 0
