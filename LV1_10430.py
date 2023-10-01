8335c27d749c34e81697466ae15b66b8fae9284e
palet404 <telap9909@gmail.com>
2023-10-01 14:29:29 +0900

    10430Try1Edit

--- LV1_10430.py
+++ LV1_10430.py
@@ -1,6 +1,9 @@
 A, B, C = map(int,input().split())
 
-print((A+B)%C)
-print(((A%C) + (B%C))%C)
-print((A*B)%C)
-print(((A%C) * (B%C))%C)
+if 2<= A and B and C <= 10**4 :
+    print((A+B)%C)
+    print(((A%C) + (B%C))%C)
+    print((A*B)%C)
+    print(((A%C) * (B%C))%C)
+else :
+    print("A,B,C should between 2 and 10,000")
